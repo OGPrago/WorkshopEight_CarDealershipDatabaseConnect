2535821a5ebb597b9c4af27a17f4210e99bec532
Jeremy <ogprago@gmail.com>
2024-06-14 16:02:16 -0400

    WIP getVehiclesByPrice()

--- src/main/java/com/ps/DAO/VehicleDAO.java
+++ src/main/java/com/ps/DAO/VehicleDAO.java
@@ -42,41 +42,69 @@ public class VehicleDAO implements VehicleInt {
                 vehicles.add(vehicle);
             }
 
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            return vehicles;
         }
-
+        return vehicles;
     }
 
     @Override
     public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        List<Vehicle> vehiclesYear = new ArrayList<>();
 
-    }
+        try (
+                Connection connection = basicDataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT * FROM vehicles WHERE price BETWEEN ? AND ? ORDER BY price");
+        ) {
+            preparedStatement.setDouble(1, min);
+            preparedStatement.setDouble(2, max);
 
-    @Override
-    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
-        return List.of();
-    }
+            try (
+                    ResultSet resultSet = preparedStatement.executeQuery();
+            ) {
+                while (resultSet.next()) {
+                    int vin = resultSet.getInt("VIN");
+                    int year = resultSet.getInt("year");
+                    String make = resultSet.getString("make");
+                    String model = resultSet.getString("model");
+                    String color = resultSet.getString("color");
+                    int mileage = resultSet.getInt("mileage");
+                    String type = resultSet.getString("type");
+                    double price = resultSet.getDouble("price");
+                    String sold = resultSet.getString("SOLD");
 
-    @Override
-    public List<Vehicle> getVehiclesByYear(int min, int max) {
-        return List.of();
-    }
+                    Vehicle vehicle = new Vehicle(vin, year, make, model, color, mileage, type, price, sold);
+                    vehiclesYear.add(vehicle);
+                }
 
-    @Override
-    public List<Vehicle> getVehiclesByColor(String color) {
-        return List.of();
-    }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
-    @Override
-    public List<Vehicle> getVehiclesByMileage(int min, int max) {
-        return List.of();
-    }
+        @Override
+        public List<Vehicle> getVehiclesByMakeModel (String make, String model){
+            return List.of();
+        }
 
-    @Override
-    public List<Vehicle> getVehiclesByType(String type) {
-        return List.of();
+        @Override
+        public List<Vehicle> getVehiclesByYear ( int min, int max){
+            return List.of();
+        }
+
+        @Override
+        public List<Vehicle> getVehiclesByColor (String color){
+            return List.of();
+        }
+
+        @Override
+        public List<Vehicle> getVehiclesByMileage ( int min, int max){
+            return List.of();
+        }
+
+        @Override
+        public List<Vehicle> getVehiclesByType (String type){
+            return List.of();
+        }
     }
-}

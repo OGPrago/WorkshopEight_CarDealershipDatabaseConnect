745fb2c72f017ac906bcc5cd2bd2255fe5fdfcc2
Jeremy <ogprago@gmail.com>
2024-06-14 11:53:08 -0400

    Created LeaseContract and SalesContract objects

--- src/main/java/com/ps/models/LeaseContract.java
+++ src/main/java/com/ps/models/LeaseContract.java
@@ -1,4 +1,46 @@
 package com.ps.models;
 
 public class LeaseContract {
+    private int leaseId;
+    private int vin;
+    private String leaseDate;
+
+    public LeaseContract(int leaseId, int vin, String leaseDate) {
+        this.leaseId = leaseId;
+        this.vin = vin;
+        this.leaseDate = leaseDate;
+    }
+
+    public int getLeaseId() {
+        return leaseId;
+    }
+
+    public void setLeaseId(int leaseId) {
+        this.leaseId = leaseId;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public String getLeaseDate() {
+        return leaseDate;
+    }
+
+    public void setLeaseDate(String leaseDate) {
+        this.leaseDate = leaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "LeaseContract{" +
+                "leaseId=" + leaseId +
+                ", vin=" + vin +
+                ", leaseDate='" + leaseDate + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/ps/models/SalesContract.java
+++ src/main/java/com/ps/models/SalesContract.java
@@ -1,4 +1,46 @@
 package com.ps.models;
 
 public class SalesContract {
+    private int salesId;
+    private int vin;
+    private String salesDate;
+
+    public SalesContract(int salesId, int vin, String salesDate) {
+        this.salesId = salesId;
+        this.vin = vin;
+        this.salesDate = salesDate;
+    }
+
+    public int getSalesId() {
+        return salesId;
+    }
+
+    public void setSalesId(int salesId) {
+        this.salesId = salesId;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public String getSalesDate() {
+        return salesDate;
+    }
+
+    public void setSalesDate(String salesDate) {
+        this.salesDate = salesDate;
+    }
+
+    @Override
+    public String toString() {
+        return "SalesContract{" +
+                "salesId=" + salesId +
+                ", vin=" + vin +
+                ", salesDate='" + salesDate + '\'' +
+                '}';
+    }
 }
